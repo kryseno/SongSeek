aa146af3275087ec21e7f27b7cb38817db9b75b4
kryseno <kryseno@gmail.com>
2018-01-13 03:39:15 -0800

    [added] id to form & click handler to submit button

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ $(document).ready(initApp);
 
 function initApp() {
     console.log('app initiated');
+    $("#submitButton").click(handleClick);
 }
 
+function handleClick() {
+    console.log('submit button clicked');
+}
