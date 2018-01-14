1ad908bf468c5b8e3db0ee8ee490255f0f8229b1
kryseno <kryseno@gmail.com>
2018-01-13 22:10:17 -0800

    [removed] commented out todos

--- main.js
+++ main.js
@@ -54,10 +54,3 @@ function sendReqToDb(obj) {
         }
     });
 };
-
-
-// ajax call
-// grab input data
-// send input data to server
-// place input data into db
-// last priority: retrieve history of requests
